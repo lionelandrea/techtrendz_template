899225dabd715d8d931e15bdb8b8007dfb19cdcc
moyo <leonboscolo200@gmail.com>
2026-01-14 16:51:43 +0100

    actualites

--- actualites.php
+++ actualites.php
@@ -5,7 +5,7 @@ require_once __DIR__ . "/lib/article.php";
 require_once __DIR__ . "/templates/header.php";
 
 // @todo On doit appeler getArticale pour récupérer les articles et faire une boucle pour les afficher
-// $articles = getArticles($pdo);
+ $articles = getArticles($pdo);
 
 ?>
 
